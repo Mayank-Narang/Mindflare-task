fce325d73f7bd6d12664cc265bbeccef8230d0a3
Mayank-Narang <mayankcodez+github@gmail.com>
2025-09-03 23:10:59 +0530

    linkedin link change

--- portfolio-mayank/src/components/Contact/Contact.jsx
+++ portfolio-mayank/src/components/Contact/Contact.jsx
@@ -20,7 +20,7 @@ export const Contact = () => {
             src={getImageUrl("contact/linkedinIcon.png")}
             alt="LinkedIn icon"
           />
-          <a href="https://www.linkedin.com/in/mayank-narang-475b93327/">Linkedin</a>
+          <a href="https://www.linkedin.com/in/mayank--narang/">Linkedin</a>
         </li>
         <li className={styles.link}>
           <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
