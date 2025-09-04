1e6ed3a7f39f4e50b635e51b726e17961a2de762
Mayank-Narang <mayankcodez+github@gmail.com>
2025-09-04 20:43:23 +0530

    Added tip jar project

--- portfolio-mayank/src/components/Projects/ProjectCard.jsx
+++ portfolio-mayank/src/components/Projects/ProjectCard.jsx
@@ -4,7 +4,7 @@ import styles from "./ProjectCard.module.css";
 import { getImageUrl } from "../../utils";
 
 export const ProjectCard = ({
-  project: { title, imageSrc, description, skills, demo, source },
+  project: { title, imageSrc, description, skills, demo, code },
 }) => {
   return (
     <div className={styles.container}>
@@ -28,8 +28,8 @@ export const ProjectCard = ({
         <a href={demo} className={styles.link}>
           Demo
         </a>
-        <a href={source} className={styles.link}>
-          Source
+        <a href={code} className={styles.link}>
+          Code
         </a>
       </div>
     </div>
